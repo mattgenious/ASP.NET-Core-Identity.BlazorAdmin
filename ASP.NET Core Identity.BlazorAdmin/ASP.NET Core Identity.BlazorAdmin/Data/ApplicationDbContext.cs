7875724a38cf7dd9c0b7678660e8712b9e8639ea
Matt Luccas Phaure Minet <matt.l.p.jensen@gmail.com>
2021-01-03 19:28:53 +0100

    New project with minor changes

---
+++ ASP.NET Core Identity.BlazorAdmin/ASP.NET Core Identity.BlazorAdmin/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASP.NET_Core_Identity.BlazorAdmin.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
